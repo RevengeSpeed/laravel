ce59360e1f082a23e0b2fc21932f0c8ad3e0938e
RevengeSpeed <victorpalomerah@hotmail.com>
2025-02-04 17:13:37 -0600

    se agrego vista para admins

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,14 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard de Administrador</title>
+</head>
+<body>
+    <h1>Bienvenido al Dashboard de Administrador</h1>
+    <p>Esta es una página exclusiva para administradores.</p>
+</body>
+</html>
